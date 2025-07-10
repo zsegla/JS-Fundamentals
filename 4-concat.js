03691a2202913dcdef4208a949579681bc83c7a9
Fekreselassie <fekruson@gmail.com>
2025-07-11 00:30:34 +0300

    fix error of file 4

--- 4-concat.js
+++ 4-concat.js
@@ -1,10 +1,6 @@
 const args = process.argv.slice(2); // Get command line arguments, excluding the first two (node and script path)
 if (args.length > 1) {
     console.log(args.join(' is ')); // Print all arguments concatenated with a space
-    }
-else if (args.length === 1) {
-    console.log(args[0]); // Print the single argument
-}
-else {
-    console.log("No arguments provided."); // Handle case where no arguments are passed
+} else {
+    console.log(`${args[0]} is undefined`); // Print the single argument
 }
