74a7ed000175e0c359c5fed4570b593ce896faf0
Fekreselassie <fekruson@gmail.com>
2025-07-11 00:47:34 +0300

    final fix

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,8 +1,6 @@
 const num = parseInt(process.argv[2]);
 
-if (isNaN(num) || num <= 0) {
-  console.error('Missing number of occurrences');
-} else {
+if (num > 0) {
   let output = '';
   for (let i = 0; i < num; i++) {
     output += 'C is fun\n';
