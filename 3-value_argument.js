fbe0074614731b093fd622a3defd9d6bc82ba595
Fekreselassie <fekruson@gmail.com>
2025-07-11 00:22:14 +0300

    fixing errors to 3

--- 3-value_argument.js
+++ 3-value_argument.js
@@ -1,12 +1,9 @@
-// Write a script that prints the first argument passed to it:
+// Write a script that prints the first argument passed to it: and not allowed to use .length
 
-const args = process.argv.slice(2); // Get command line arguments, excluding the first two (node and script path)
-if (args.length > 0) {
-  console.log(args[0]); // Print the first argument
+const arg = process.argv[2];
+
+if (arg !== undefined) {
+  console.log(arg);
+} else {
+  console.log("No argument");
 }
-else {
-  console.log("No arguments provided."); // Handle case where no arguments are passed
-}
-// Usage: node script.js arg1 arg2 ...
-// Example: If you run `node script.js hello world`, it will print "hello".
-// Note: This script is designed to be run in a Node.js environment.
